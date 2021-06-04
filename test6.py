c243d4f5750e5b3289c7955c65f8bc0e23b7a0f7
funksk <mkforever200@gmail.com>
2021-06-04 19:45:22 -0400

    it works like mostly with the bass clef thing. add stuff like optional bass clef stuff or cmd line args

--- test6.py
+++ test6.py
@@ -43,14 +43,26 @@ c + t1
 #bar = lilypond.from_Bar(b)
 #track = lilypond.from_Track(t)
 composition = lilypond.from_Composition(c)
-i = j = 0
-while(composition[i] != '{' or j != 2):
-    if(composition[i] == '{'):
-        j+=1
+print('prefuckery ', composition)
+i = 0
+j = 2
+li = []
+for x in (range(0,len(composition))):
+    if (composition[i] == '{' and composition[j] == '{' and composition[j-1] == ' '):
+        li.append([i,j])
     i+=1
-print(i, j)
-print(composition)
-composition = composition[:i] + "\clef bass " + composition[i:]
-print(composition)
+    j+=1
+
+#print(i, j)
+print(f'li = {li}')
+#the reason for the +(13*x) is because 13 is the amount of space that the string adds, and we need to add it to
+#the end of the file anyways. however many times we have instruments or it is triggered, we must add it there.
+#indexing issue
+for x in range(0,len(li)):
+   composition = composition[:li[x][0]+(13*x)] + "{ \clef bass " + composition[li[x][1]+(13*x):] 
+print('postfuckery', composition)
+
+print('***********<lilypond starting>************')
 lilypond.to_png(composition, "test")
+print('*********<nomacs starting>*********')
 os.system('nomacs test.png')
